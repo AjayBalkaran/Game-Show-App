9b97f8821552b2f19518a80337e860286cad3bd7
Ajay Balkaran <ajaybalkaran@gmail.com>
2019-11-07 14:35:06 -0500

    Update app.js
    Attached a event listener to the Start Game button to hide the Start Screen overlay

--- js/app.js
+++ js/app.js
@@ -1,4 +1,11 @@
 const qwerty = document.querySelector('#qwerty');
 const phrase = document.querySelector('#phrase');
+const overlay = document.querySelector('#overlay');
 // Missed is the life counter 
 let missed = 0; 
+
+overlay.addEventListener('click', (e) => {
+    if (e.target.className === 'btn__reset'){
+        e.target.parentNode.style.display = 'none';
+    }
+});
