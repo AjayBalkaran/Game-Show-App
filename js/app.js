60275c07166a19d7fc17ab874577a2bf4a5904df
Ajay Balkaran <ajaybalkaran@gmail.com>
2019-11-07 14:40:24 -0500

    Update app.js
    Created a phrases array that contains at least 5 different phrases as strings.

--- js/app.js
+++ js/app.js
@@ -1,8 +1,19 @@
 const qwerty = document.querySelector('#qwerty');
 const phrase = document.querySelector('#phrase');
 const overlay = document.querySelector('#overlay');
+
+//Phrases that the game will use 
+const phrases = [
+    "A GAME OF CAT AND MOUSE",
+    "A ONCE IN A LIFETIME OPPORTUNITY",
+    "FALL IN LOVE WITH THIS CITY",
+    "I SEE WHERE THIS IS GOING",
+    "MY HEAD IS IN THE CLOUDS",
+];
+
 // Missed is the life counter 
 let missed = 0; 
+
 
 overlay.addEventListener('click', (e) => {
     if (e.target.className === 'btn__reset'){
